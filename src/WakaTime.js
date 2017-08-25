1c6b2b9532e81a1aeec390873d250d1ea206feb3
Alexander Lichter <manniL@gmx.net>
2017-08-25 19:47:26 +0200

    Rewrite library to ES6
    + Add testing with Mocha and Chai
    + Replace request with Axios for promise-based solution
    + Split up code into several files
    + Add default value for "stats" (7 days)
    + Add npm scripts and babel to create ES5 version (for NodeJS)

---
+++ src/WakaTime.js
@@ -0,0 +1,80 @@
+import btoa from 'btoa'
+import * as axios from 'axios'
+import { serialize, getDateString } from './helpers'
+import { BASE_URL } from './constants'
+
+export class WakaTime {
+
+  constructor (apiKey) {
+    this.apiKey = apiKey
+  }
+
+  getApiOptions (path, query) {
+    const qs = query ? serialize(query) : ''
+    return {
+      url: BASE_URL + path + qs,
+      headers: {
+        'Authorization': 'Basic ' + btoa(this.apiKey)
+      }
+    }
+  }
+
+  setApiKey (apiKey) {
+    this.apiKey = apiKey
+  }
+
+  getApiKey () {
+    return this.apiKey
+  }
+
+  async currentUser () {
+    console.log(this.getApiOptions('/users/current'))
+    const {url, headers} = this.getApiOptions('/users/current')
+    console.log(url, headers)
+    return await axios.get(url, {headers})
+  }
+
+  async stats (stat = 'last_7_days') {
+    const validRanges = [
+      'last_7_days',
+      'last_30_days',
+      'last_6_months',
+      'last_year',
+      'all_time',
+    ]
+    if (!validRanges.includes(stat)) {
+      return new Error('Invalid stat parameter')
+    }
+    const {url, headers} = this.getApiOptions('/users/current/stats/' + stat)
+    return await axios.get(url, {headers})
+
+  }
+
+  async summaries (object) {
+    let start = ''
+    let end = ''
+
+    if (typeof object === 'object' && !(object instanceof Date)) {
+      start = getDateString(object.start)
+      end = getDateString(object.end)
+    } else {
+      start = getDateString(object)
+      end = getDateString(object)
+    }
+
+    const {url, headers} = this.getApiOptions('/users/current/summaries', {
+      start: start,
+      end: end
+    })
+
+    return await axios.get(url, {headers})
+  }
+
+  async durations (date) {
+    const {url, headers} = this.getApiOptions('/users/current/durations', {
+      date: getDateString(date)
+    })
+    return await axios.get(url, {headers})
+
+  }
+}
---
+++ src/WakaTime.test.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai'
+
+import { WakaTime } from './WakaTime'
+import { describe, it } from 'mocha'
+
+describe('WakaTime', () => {
+  it('should return API key after being initialized', () => {
+    const API_KEY = 'abcde'
+    const instance = new WakaTime(API_KEY)
+    expect(instance.getApiKey()).to.equal(API_KEY)
+  })
+  it('should return API key after manually setting it', () => {
+    const API_KEY = 'abcde'
+    const instance = new WakaTime()
+    instance.setApiKey(API_KEY)
+    expect(instance.getApiKey()).to.equal(API_KEY)
+  })
+  it('should return an promise error when calling API methods', (done) => {
+    const API_KEY = 'abcde'
+    const instance = new WakaTime(API_KEY)
+    const result = instance.stats()
+    result
+      .catch(() => {
+        /*
+        Getting unauthorized because no valid API key
+        But at least it's a promise that worked!
+         */
+        done()
+      })
+  })
+})
