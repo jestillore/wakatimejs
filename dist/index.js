83830be1c4432629e6cf9b7f42afb5c3880a24cc
Alexander Lichter <manniL@gmx.net>
2017-08-25 22:22:22 +0200

    Add Date prototype changes

--- dist/index.js
+++ dist/index.js
@@ -15,3 +15,11 @@ Object.keys(_WakaTime).forEach(function (key) {
     }
   });
 });
+
+
+Date.prototype.wakaTimeDateString = function () {
+  var yyyy = this.getFullYear().toString();
+  var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
+  var dd = this.getDate().toString();
+  return yyyy + '-' + (mm[1] ? mm : '0' + mm[0]) + '-' + (dd[1] ? dd : '0' + dd[0]); // padding
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 export * from './WakaTime'
 
+Date.prototype.wakaTimeDateString = function () {
+  const yyyy = this.getFullYear().toString()
+  const mm = (this.getMonth() + 1).toString() // getMonth() is zero-based
+  const dd = this.getDate().toString()
+  return yyyy + '-' + (mm[1] ? mm : '0' + mm[0]) + '-' + (dd[1] ? dd : '0' + dd[0]) // padding
+}
